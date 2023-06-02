30b6ebb233c3af57c1f7100fee6bce152ad1af65
Negin <negin.khademi@gmail.com>
2023-06-02 15:54:12 +0330

    complete first project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import ProfileCard from './ProfileCard';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section ">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@Alexa33"
+                image={AlexaImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@Cortana2023"
+                image={CortanaImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@Siri22"
+                image={SiriImage}
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
